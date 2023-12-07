11556da2a93dd6432799725ef37fe3df9a2f1cf6
wy404 <18669633238@163.com>
2023-12-08 01:27:25 +0800

    feat:'工作经历'

--- App.jsx
+++ App.jsx
@@ -1,11 +1,14 @@
+import Expertise from "./components/Expertise/Expertise";
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
+import Work from './components/Work/Work'
 import css from './styles/app.module.scss'
 const App = () => {
   //don't forget to add font link in index.html
   return <div className={`bg-primary ${css.container}`}>
     <Header/>
     <Hero/>
+    <Work/>
   </div>;
 };
 
